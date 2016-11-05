c4a623d51bafff9b4b88e0d927be03cca60486ed
Pere Orga <pere@orga.cat>
2016-11-05 16:45:06 +0100

    js: ensure a maximum of 1 request at once and handle network errors

--- script.js
+++ script.js
@@ -2,13 +2,24 @@
 
 function uploadContent() {
     if (content !== textarea.value) {
-        content = textarea.value;
+        var temp = textarea.value;
         var request = new XMLHttpRequest();
         request.open('POST', window.location.href, true);
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-        request.send("t=" + encodeURIComponent(content));
+        request.onload = function() {
+            if (request.readyState === 4) {
+                content = temp;
+                setTimeout(uploadContent, 1000);
+            }
+        }
+        request.onerror = function() {
+            setTimeout(uploadContent, 1000);
+        }
+        request.send("t=" + encodeURIComponent(temp));
     }
-    setTimeout(uploadContent, 1000);
+    else {
+        setTimeout(uploadContent, 1000);
+    }
 }
 
 var textarea = document.getElementById('content');
