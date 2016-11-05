30c62f100157fa84e404dc296161b8adb44febdd
Pere Orga <pere@orga.cat>
2016-11-05 10:22:20 +0100

    js: replace setInterval() with setTimeout()

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var textarea = document.getElementById('content');
-var content = textarea.value;
-
-textarea.focus();
-
-setInterval(function() {
+function uploadContent() {
     if (content !== textarea.value) {
-
         content = textarea.value;
         var request = new XMLHttpRequest();
         request.open('POST', window.location.href, true);
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
         request.send("t=" + encodeURIComponent(content));
     }
-}, 1000);
+    setTimeout(uploadContent, 1000);
+}
+
+var textarea = document.getElementById('content');
+var content = textarea.value;
+textarea.focus();
+uploadContent();
