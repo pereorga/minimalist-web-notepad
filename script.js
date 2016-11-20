3b6f5549acdf28fca34d511d3089ec8b3bc21eef
Pere Orga <pere@orga.cat>
2016-11-20 10:25:20 +0100

    Improve comments and spacing

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 /*! Minimalist Web Notepad | https://github.com/pereorga/minimalist-web-notepad */
 
 function uploadContent() {
+
     if (content !== textarea.value) {
+
+        // Text area value has changed.
         var temp = textarea.value;
         var request = new XMLHttpRequest();
         request.open('POST', window.location.href, true);
         request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
         request.onload = function() {
+
             if (request.readyState === 4) {
+
+                // Request has ended, check again after 1 second.
                 content = temp;
                 setTimeout(uploadContent, 1000);
             }
         }
+
         request.onerror = function() {
+
+            // On connection error, try again after 1 second.
             setTimeout(uploadContent, 1000);
         }
+
+        // Send the request.
         request.send("t=" + encodeURIComponent(temp));
 
         // Make the content available to print.
@@ -22,6 +34,8 @@ function uploadContent() {
         printable.appendChild(document.createTextNode(temp));
     }
     else {
+
+        // Content has not changed, check again after 1 second.
         setTimeout(uploadContent, 1000);
     }
 }
@@ -30,6 +44,9 @@ var textarea = document.getElementById('content');
 var printable = document.getElementById('printable');
 var content = textarea.value;
 
+// Make the content available to print.
 printable.appendChild(document.createTextNode(content));
+
 textarea.focus();
+
 uploadContent();
