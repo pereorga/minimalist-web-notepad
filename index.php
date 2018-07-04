369479a5c7404b82c01702ac1e1d16d032f1dfe2
Pere Orga <pereorga@users.noreply.github.com>
2018-07-04 22:58:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -25,11 +25,9 @@ if (empty($_GET['f']) || sanitizeString($_GET['f']) !== $_GET['f']) {
 
     // User has not specified a valid name, generate one.
     // Initially based on http://stackoverflow.com/a/4356295/1391963
-
     // Do not generate ambiguous characters. See http://ux.stackexchange.com/a/53345/25513
     $characters = '23456789abcdefghjkmnpqrstuvwxyzABCDEFGHJKMNPQRSTUVWXYZ';
     $randomString = '';
-
     $length = 5;
     for ($i = 0; $i < $length; ++$i) {
         $randomString .= $characters[mt_rand(0, strlen($characters) - 1)];
