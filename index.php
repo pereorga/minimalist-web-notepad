209a43e46d973e7ec056874b38d3ddf40abdb1ff
Pere Orga <pereorga@users.noreply.github.com>
2020-03-24 21:32:18 +0100

    Cosmetics

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Base URL of the website, without trailing slash.
-$base_url =  getenv('MWN_BASE_URL') ?: 'https://notes.orga.cat';
+$base_url = getenv('MWN_BASE_URL') ?: 'https://notes.orga.cat';
 
 // Path to the directory to save the notes in, without trailing slash.
 // Should be outside of the document root, if possible.
