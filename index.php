c67e14b7b748e70f7149cca45ccfe4db09e1a847
Pere Orga <pere@orga.cat>
2021-10-22 00:37:11 +0200

    index.php: remove generator tag

--- index.php
+++ index.php
@@ -49,7 +49,6 @@ if (isset($_GET['raw']) || strpos($_SERVER['HTTP_USER_AGENT'], 'curl') === 0 || 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="generator" content="Minimalist Web Notepad (https://github.com/pereorga/minimalist-web-notepad)">
     <title><?php print $_GET['note']; ?></title>
     <link rel="icon" href="<?php print $base_url; ?>/favicon.svg" type="image/svg+xml">
     <link rel="stylesheet" href="<?php print $base_url; ?>/styles.css">
