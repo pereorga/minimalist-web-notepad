8ce8e95b8db7f2cfad0877ec59ed732e4db3e6f7
Pere Orga <pere@orga.cat>
2023-03-04 11:10:33 +0100

    php: simplify cache headers

--- index.php
+++ index.php
@@ -8,9 +8,7 @@ $base_url = 'https://notes.orga.cat';
 $save_path = '_tmp';
 
 // Disable caching.
-header('Cache-Control: no-cache, no-store, must-revalidate');
-header('Pragma: no-cache');
-header('Expires: 0');
+header('Cache-Control: no-store');
 
 // If no note name is provided, or if the name is too long, or if it contains invalid characters.
 if (!isset($_GET['note']) || strlen($_GET['note']) > 64 || !preg_match('/^[a-zA-Z0-9_-]+$/', $_GET['note'])) {
