628e27cf1b6b2cf14b29af4328d8963a0e19f629
Sergio Giraldo <sergio@giraldo.com.br>
2023-07-05 12:20:59 +0200

    Allows usage of folder structure and shows an outline of filesystem on right pane:: by sergio giraldo @ 20230705T1220CEST, gpg signed

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Base URL of the website, without trailing slash.
-$base_url = 'https://notes.orga.cat';
+$base_url = 'https://www.example.com';
 
 // Path to the directory to save the notes in, without trailing slash.
 // Should be outside the document root, if possible.
@@ -10,8 +10,35 @@ $save_path = '_tmp';
 // Disable caching.
 header('Cache-Control: no-store');
 
+// logic used
+// if has /(slash)
+// // if ends in slash, consider it a full path and create a random filename
+// // if not ends in slash, consider last part is the filename
+// else
+// if has a name, consider it the filename
+//
+// tests
+// @/ -> new file 
+// @/file1 -> open file1 
+// @/folder1/ -> creates folder1 and new file
+// @/folder1/file1 -> open file1 from folder1 OK
+// @/folder1/folder2/ -> creates folder1/folder2 and new file
+// @/folder1/folder2/file1 -> open file1 from folder1/folder2
+//
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+//  RUNS     |           @           |        @/file1        |       @/folder1/      |   @/folder1/file1    |  @/folder1/folder2/  | @/folder1/folder2/file1 |
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+//   1       |           x           |           x           |                      |           x           |          x           |           x             |   
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+//   2       |           x           |                       |                      |                       |                      |                         |
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+//   3       |           x           |           x           |          x           |           x           |          x           |           x             |
+//-----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
 // If no note name is provided, or if the name is too long, or if it contains invalid characters.
-if (!isset($_GET['note']) || strlen($_GET['note']) > 64 || !preg_match('/^[a-zA-Z0-9_-]+$/', $_GET['note'])) {
+if (!isset($_GET['note']) || strlen($_GET['note']) > 64 || !preg_match('/^[a-zA-Z0-9_\/-]+$/', $_GET['note'])) {
 
     // Generate a name with 5 random unambiguous characters. Redirect to it.
     header("Location: $base_url/" . substr(str_shuffle('234579abcdefghjkmnpqrstwxyz'), -5));
@@ -20,8 +47,25 @@ if (!isset($_GET['note']) || strlen($_GET['note']) > 64 || !preg_match('/^[a-zA-
 
 $path = $save_path . '/' . $_GET['note'];
 
-if (isset($_POST['text'])) {
+if (preg_match('/\//', $_GET['note'])) { //folder structure
+    if (preg_match('/\/$/', $_GET['note'])) { //end in slash
+        $newdir = $_GET['note'];
+        if (!is_dir($newdir)) {
+            mkdir($newdir, 0777, true);
+        }
+        // Generate a name with 5 random unambiguous characters. Redirect to it.
+        header("Location: $base_url/" . $_GET['note'] . substr(str_shuffle('234579abcdefghjkmnpqrstwxyz'), -5));
+        die;
+    }
+    else{
+        $newdir = (dirname($path) . '/');
+        if (!is_dir($newdir)) {
+            mkdir($newdir, 0777, true);
+        }
+    }
+}
 
+if (isset($_POST['text'])) {
     // Update file.
     file_put_contents($path, $_POST['text']);
 
@@ -42,6 +86,36 @@ if (isset($_GET['raw']) || strpos($_SERVER['HTTP_USER_AGENT'], 'curl') === 0 || 
     }
     die;
 }
+
+function displayFolderTree($directory, $indentation = '') {
+    $files = scandir($directory);
+
+    // Separate directories and files into two arrays
+    $dirs = [];
+    $other = [];
+
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..') {
+            if (is_dir($directory . '/' . $file)) {
+                $dirs[] = $file;
+            } else {
+                $other[] = $file;
+            }
+        }
+    }
+
+    // Handle directories first
+    foreach ($dirs as $dir) {
+        print $indentation . '- <b>' . $dir . "</b><br />";
+        displayFolderTree($directory . '/' . $dir, $indentation . '--');
+    }
+
+    // Handle files next
+    foreach ($other as $file) {
+        print $indentation . '- ' . $file . "<br />";
+    }
+}
+
 ?><!DOCTYPE html>
 <html>
 <head>
@@ -50,17 +124,24 @@ if (isset($_GET['raw']) || strpos($_SERVER['HTTP_USER_AGENT'], 'curl') === 0 || 
     <title><?php print $_GET['note']; ?></title>
     <link rel="icon" href="<?php print $base_url; ?>/favicon.ico" sizes="any">
     <link rel="icon" href="<?php print $base_url; ?>/favicon.svg" type="image/svg+xml">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php print $base_url; ?>/styles.css">
 </head>
 <body>
-    <div class="container">
-        <textarea id="content"><?php
-            if (is_file($path)) {
-                print htmlspecialchars(file_get_contents($path), ENT_QUOTES, 'UTF-8');
-            }
-        ?></textarea>
-    </div>
-    <pre id="printable"></pre>
+        <div class="left">
+            <span>
+                <?php
+                    $rootDirectory = '/home/giral/www/dontpad/_tmp';
+                    displayFolderTree($rootDirectory);
+                ?>
+            </span>
+        </div>
+        <div class="right">
+            <textarea id="content"><?php if (is_file($path)) {print htmlspecialchars(file_get_contents($path), ENT_QUOTES, 'UTF-8');}?></textarea>
+            <pre id="printable"></pre>
+        </div>
     <script src="<?php print $base_url; ?>/script.js"></script>
 </body>
 </html>
