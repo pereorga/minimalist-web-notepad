f2cff0fc59bc8ef1e21bc2cdf593e07d0c81b8a6
Sergio Giraldo <sergio@giraldo.com.br>
2023-07-07 21:02:37 +0200

    better folder tree:: by sergio giraldo @ 20230707T2102CEST, gpg signed

--- index.php
+++ index.php
@@ -8,18 +8,18 @@ $base_url = 'https://www.example.com';
 $save_path = '_tmp';
 
 // Disable caching.
-header('Cache-Control: no-store');
+header( 'Cache-Control: no-store' );
 
 // logic used
-// if has /(slash)
+// if has /( slash )
 // // if ends in slash, consider it a full path and create a random filename
 // // if not ends in slash, consider last part is the filename
 // else
 // if has a name, consider it the filename
 //
 // tests
-// @/ -> new file 
-// @/file1 -> open file1 
+// @/ -> new file
+// @/file1 -> open file1
 // @/folder1/ -> creates folder1 and new file
 // @/folder1/file1 -> open file1 from folder1 OK
 // @/folder1/folder2/ -> creates folder1/folder2 and new file
@@ -28,120 +28,178 @@ header('Cache-Control: no-store');
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 //  RUNS     |           @           |        @/file1        |       @/folder1/      |   @/folder1/file1    |  @/folder1/folder2/  | @/folder1/folder2/file1 |
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
-//   1       |           x           |           x           |                      |           x           |          x           |           x             |   
+//   1       |           x           |           x           |                      |           x           |          x           |           x             |
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 //   2       |           x           |                       |                      |                       |                      |                         |
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 //   3       |           x           |           x           |          x           |           x           |          x           |           x             |
 //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
-
-
 // If no note name is provided, or if the name is too long, or if it contains invalid characters.
-if (!isset($_GET['note']) || strlen($_GET['note']) > 64 || !preg_match('/^[a-zA-Z0-9_\/-]+$/', $_GET['note'])) {
+if ( !isset( $_GET[ 'note' ] ) || strlen( $_GET[ 'note' ] ) > 64 || !preg_match( '/^[a-zA-Z0-9_\/-]+$/', $_GET[ 'note' ] ) ) {
 
     // Generate a name with 5 random unambiguous characters. Redirect to it.
-    header("Location: $base_url/" . substr(str_shuffle('234579abcdefghjkmnpqrstwxyz'), -5));
+    header( "Location: $base_url/" . substr( str_shuffle( '234579abcdefghjkmnpqrstwxyz' ), -5 ) );
     die;
 }
 
-$path = $save_path . '/' . $_GET['note'];
+$path = $save_path . '/' . $_GET[ 'note' ];
 
-if (preg_match('/\//', $_GET['note'])) { //folder structure
-    if (preg_match('/\/$/', $_GET['note'])) { //end in slash
-        $newdir = $_GET['note'];
-        if (!is_dir($newdir)) {
-            mkdir($newdir, 0777, true);
+if ( preg_match( '/\//', $_GET[ 'note' ] ) ) {
+    //folder structure
+    if ( preg_match( '/\/$/', $_GET[ 'note' ] ) ) {
+        //end in slash
+        $newdir = $_GET[ 'note' ];
+        if ( !is_dir( $newdir ) ) {
+            mkdir( $newdir, 0777, true );
         }
         // Generate a name with 5 random unambiguous characters. Redirect to it.
-        header("Location: $base_url/" . $_GET['note'] . substr(str_shuffle('234579abcdefghjkmnpqrstwxyz'), -5));
+        header( "Location: $base_url/" . $_GET[ 'note' ] . substr( str_shuffle( '234579abcdefghjkmnpqrstwxyz' ), -5 ) );
         die;
-    }
-    else{
-        $newdir = (dirname($path) . '/');
-        if (!is_dir($newdir)) {
-            mkdir($newdir, 0777, true);
+    } else {
+        $newdir = ( dirname( $path ) . '/' );
+        if ( !is_dir( $newdir ) ) {
+            mkdir( $newdir, 0777, true );
         }
     }
 }
 
-if (isset($_POST['text'])) {
+if ( isset( $_POST[ 'text' ] ) ) {
     // Update file.
-    file_put_contents($path, $_POST['text']);
+    file_put_contents( $path, $_POST[ 'text' ] );
 
     // If provided input is empty, delete file.
-    if (!strlen($_POST['text'])) {
-        unlink($path);
+    if ( !strlen( $_POST[ 'text' ] ) ) {
+        unlink( $path );
     }
     die;
 }
 
 // Print raw file when explicitly requested, or if the client is curl or wget.
-if (isset($_GET['raw']) || strpos($_SERVER['HTTP_USER_AGENT'], 'curl') === 0 || strpos($_SERVER['HTTP_USER_AGENT'], 'Wget') === 0) {
-    if (is_file($path)) {
-        header('Content-type: text/plain');
-        readfile($path);
+if ( isset( $_GET[ 'raw' ] ) || strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'curl' ) === 0 || strpos( $_SERVER[ 'HTTP_USER_AGENT' ], 'Wget' ) === 0 ) {
+    if ( is_file( $path ) ) {
+        header( 'Content-type: text/plain' );
+        readfile( $path );
     } else {
-        header('HTTP/1.0 404 Not Found');
+        header( 'HTTP/1.0 404 Not Found' );
     }
     die;
 }
 
-function displayFolderTree($directory, $indentation = '') {
-    $files = scandir($directory);
+// Function to sort given values alphabetically
+function alphasort( $a, $b ) {
+    $specialChars = array( "$", '-', '_', '.', '+', '!', '*', "'", '(', ')', ',' );
+    return strcasecmp( str_replace( $specialChars, '\\', $a->getPathname() ), str_replace( $specialChars, '\\', $b->getPathname() ) );
+}
 
-    // Separate directories and files into two arrays
-    $dirs = [];
-    $other = [];
+// Class to put forward the values for sorting
+class SortingIterator implements IteratorAggregate {
+    private $iterator = null;
 
-    foreach ($files as $file) {
-        if ($file !== '.' && $file !== '..') {
-            if (is_dir($directory . '/' . $file)) {
-                $dirs[] = $file;
-            } else {
-                $other[] = $file;
-            }
-        }
+    public function __construct( Traversable $iterator, $callback ) {
+        $array = iterator_to_array( $iterator );
+        usort( $array, $callback );
+        $this->iterator = new ArrayIterator( $array );
     }
 
-    // Handle directories first
-    foreach ($dirs as $dir) {
-        print $indentation . '- <b>' . $dir . "</b><br />";
-        displayFolderTree($directory . '/' . $dir, $indentation . '--');
-    }
-
-    // Handle files next
-    foreach ($other as $file) {
-        print $indentation . '- ' . $file . "<br />";
+    public function getIterator() {
+        return $this->iterator;
     }
 }
 
-?><!DOCTYPE html>
+function showTree( $path ) {
+    $excludeFileFolder = '..';
+
+    $objectList = new SortingIterator( new RecursiveIteratorIterator( new RecursiveDirectoryIterator( $path ), RecursiveIteratorIterator::SELF_FIRST ), 'alphasort' );
+
+    // With that done, create arrays for out final ordered list and a temp array of files to copy over
+    $finalArray = $tempArray =  array();
+
+    // To start, push folders from object into finalArray, files into tempArray
+    foreach ( $objectList as $objectRef ) {
+        $fileFolderName = rtrim( substr( $objectRef->getPathname(), strlen( $path ) ) );
+        if ( $objectRef->getFilename() != '.' && $fileFolderName[ strlen( $fileFolderName )-1 ] != '/' && !strpos( $fileFolderName, $excludeFileFolder ) ) {
+            $fileFolderName != '/' && is_dir( $path.$fileFolderName ) ? array_push( $finalArray, $fileFolderName ) : array_push( $tempArray, $fileFolderName );
+        }
+    }
+
+    // Now push root files onto the end of finalArray and splice from the temp, leaving only files that reside in subsirs
+    for ( $i = 0; $i<count( $tempArray );
+    $i++ ) {
+        if ( count( explode( '/', $tempArray[ $i ] ) ) == 2 ) {
+            array_push( $finalArray, $tempArray[ $i ] );
+            array_splice( $tempArray, $i, 1 );
+            $i--;
+        }
+    }
+
+    // Lastly we push remaining files into the right subdirs in finalArray
+    for ( $i = 0; $i<count( $tempArray );
+    $i++ ) {
+        $insertAt = array_search( dirname( $tempArray[ $i ] ), $finalArray )+1;
+        for ( $j = $insertAt; $j<count( $finalArray );
+        $j++ ) {
+            if ( 	strcasecmp( dirname( $finalArray[ $j ] ), dirname( $tempArray[ $i ] ) ) == 0 &&
+            strcasecmp( basename( $finalArray[ $j ] ), basename( $tempArray[ $i ] ) )<0 ||
+            strstr( dirname( $finalArray[ $j ] ), dirname( $tempArray[ $i ] ) ) ) {
+                $insertAt++;
+            }
+        }
+        array_splice( $finalArray, $insertAt, 0, $tempArray[ $i ] );
+    }
+
+    // Finally, we have our ordered list, so display in a UL
+    echo '<ul>\n<li>/</li>\n';
+    $lastPath = '';
+    for ( $i = 0; $i<count( $finalArray );
+    $i++ ) {
+        $fileFolderName = $finalArray[ $i ];
+        $thisDepth = count( explode( '/', $fileFolderName ) );
+        $lastDepth = count( explode( '/', $lastPath ) );
+        if ( $thisDepth > $lastDepth ) {
+            echo '<ul>\n';
+        }
+        if ( $thisDepth < $lastDepth ) {
+            for ( $j = $lastDepth; $j>$thisDepth; $j-- ) {
+                echo '</ul>\n';
+            }
+        }
+        echo '<li>'.basename( $fileFolderName ).'</li>\n';
+        $lastPath = $fileFolderName;
+    }
+    echo '</ul>\n</ul>\n';
+}
+
+?>
+<!DOCTYPE html>
 <html>
+
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?php print $_GET['note']; ?></title>
-    <link rel="icon" href="<?php print $base_url; ?>/favicon.ico" sizes="any">
-    <link rel="icon" href="<?php print $base_url; ?>/favicon.svg" type="image/svg+xml">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php print $base_url; ?>/styles.css">
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <title><?php print $_GET[ 'note' ];?></title>
+    <link rel='icon' href="<favicon.ico" sizes='any'>
+    <link rel='icon' href="favicon.svg" type='image/svg+xml'>
+    <link rel='preconnect' href='https://fonts.googleapis.com'>
+    <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
+    <link href='https://fonts.googleapis.com/css2?family=Roboto&display=swap' rel='stylesheet'>
+    <link rel='stylesheet' href="styles.css">
 </head>
+
 <body>
-        <div class="left">
-            <span>
-                <?php
-                    $rootDirectory = '/home/giral/www/dontpad/_tmp';
-                    displayFolderTree($rootDirectory);
+    <div class='left'>
+        <span>
+            <?php
+                $rootDirectory = '/path/your/folder/_tmp';
+                showTree( $rootDirectory );
                 ?>
-            </span>
-        </div>
-        <div class="right">
-            <textarea id="content"><?php if (is_file($path)) {print htmlspecialchars(file_get_contents($path), ENT_QUOTES, 'UTF-8');}?></textarea>
-            <pre id="printable"></pre>
-        </div>
-    <script src="<?php print $base_url; ?>/script.js"></script>
+        </span>
+    </div>
+    <div class='right'>
+        <textarea id='content'><?php if ( is_file( $path ) ) {print htmlspecialchars( file_get_contents( $path ), ENT_QUOTES, 'UTF-8' );}?></textarea>
+        <pre id='printable'></pre>
+    </div>
+    <script src="script.js"></script>
 </body>
+
 </html>
