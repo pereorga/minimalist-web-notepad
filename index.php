056bd5bb08f6d25739212296617ff4036ba622f9
Pere Orga <pere@orga.cat>
2024-06-27 00:09:39 +0200

    remove old favicon.ico

--- index.php
+++ index.php
@@ -45,7 +45,6 @@ if (isset($_GET['raw']) || strpos($_SERVER['HTTP_USER_AGENT'], 'curl') === 0 || 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php print $_GET['note']; ?></title>
-<link rel="icon" href="favicon.ico" sizes="any">
 <link rel="icon" href="favicon.svg" type="image/svg+xml">
 <style>
 body {
